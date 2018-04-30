cced790d7b73515976cc2b83616e21634729e6ac
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-04-30 22:10:32 +0300

    initialize() and show() methods for QtJambi initialization

--- src/com/benovskyi/bohdan/Client.java
+++ src/com/benovskyi/bohdan/Client.java
@@ -1,10 +1,21 @@
 package com.benovskyi.bohdan;
 
-public class Client {
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QWidget;
 
+public class Client extends QWidget{
+
+	public Client() {
+		show();
+	}
+	
 	public static void main(String[] args) {
+		QApplication.initialize(args);
 		
-
+		Client client = new Client();
+		
+		
+		QApplication.execStatic();
 	}
 
 }
