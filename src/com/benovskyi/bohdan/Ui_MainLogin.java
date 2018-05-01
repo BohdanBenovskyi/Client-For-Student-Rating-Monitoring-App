540ccdf5b97f50dc38f7c7f9d61f4edaa0f37841
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-05-01 16:57:42 +0300

    compiled UI by JUIC to java class

--- src/com/benovskyi/bohdan/Ui_MainLogin.java
+++ src/com/benovskyi/bohdan/Ui_MainLogin.java
@@ -1,3 +1,11 @@
+/********************************************************************************
+ ** Form generated from reading ui file 'MainLogin.jui'
+ **
+ ** Created by: Qt User Interface Compiler version 4.8.7
+ **
+ ** WARNING! All changes made in this file will be lost when recompiling ui file!
+ ********************************************************************************/
+package com.benovskyi.bohdan;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
 
@@ -5,14 +13,14 @@ public class Ui_MainLogin implements com.trolltech.qt.QUiForm<QWidget>
 {
     public QGridLayout gridLayout;
     public QVBoxLayout verticalLayout_3;
-    public QHBoxLayout horizontalLayout_2;
+    public QHBoxLayout horizontalLayout;
     public QVBoxLayout verticalLayout;
     public QLabel lblLogin;
     public QLabel lblPassword;
     public QVBoxLayout verticalLayout_2;
     public QLineEdit edtLogin;
     public QLineEdit edtPassword;
-    public QHBoxLayout horizontalLayout;
+    public QHBoxLayout horizontalLayout_2;
     public QPushButton btnLogin;
     public QPushButton btnExit;
 
@@ -21,19 +29,19 @@ public class Ui_MainLogin implements com.trolltech.qt.QUiForm<QWidget>
     public void setupUi(QWidget MainLogin)
     {
         MainLogin.setObjectName("MainLogin");
-        MainLogin.resize(new QSize(275, 105).expandedTo(MainLogin.minimumSizeHint()));
+        MainLogin.resize(new QSize(259, 113).expandedTo(MainLogin.minimumSizeHint()));
         QSizePolicy sizePolicy = new QSizePolicy(com.trolltech.qt.gui.QSizePolicy.Policy.Fixed, com.trolltech.qt.gui.QSizePolicy.Policy.Fixed);
         sizePolicy.setHorizontalStretch((byte)0);
         sizePolicy.setVerticalStretch((byte)0);
         sizePolicy.setHeightForWidth(MainLogin.sizePolicy().hasHeightForWidth());
         MainLogin.setSizePolicy(sizePolicy);
-        MainLogin.setWindowIcon(new QIcon(new QPixmap("../../../../resources/220px-Polytechnic_Lwow_20091.bmp")));
+        MainLogin.setWindowIcon(new QIcon(new QPixmap("D:\\Projects\\ClientAppSRM\\resources\\220px-Polytechnic_Lwow_20091.bmp")));
         gridLayout = new QGridLayout(MainLogin);
         gridLayout.setObjectName("gridLayout");
         verticalLayout_3 = new QVBoxLayout();
         verticalLayout_3.setObjectName("verticalLayout_3");
-        horizontalLayout_2 = new QHBoxLayout();
-        horizontalLayout_2.setObjectName("horizontalLayout_2");
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout.setObjectName("horizontalLayout");
         verticalLayout = new QVBoxLayout();
         verticalLayout.setObjectName("verticalLayout");
         lblLogin = new QLabel(MainLogin);
@@ -47,7 +55,7 @@ public class Ui_MainLogin implements com.trolltech.qt.QUiForm<QWidget>
         verticalLayout.addWidget(lblPassword);
 
 
-        horizontalLayout_2.addLayout(verticalLayout);
+        horizontalLayout.addLayout(verticalLayout);
 
         verticalLayout_2 = new QVBoxLayout();
         verticalLayout_2.setObjectName("verticalLayout_2");
@@ -62,25 +70,25 @@ public class Ui_MainLogin implements com.trolltech.qt.QUiForm<QWidget>
         verticalLayout_2.addWidget(edtPassword);
 
 
-        horizontalLayout_2.addLayout(verticalLayout_2);
+        horizontalLayout.addLayout(verticalLayout_2);
 
 
-        verticalLayout_3.addLayout(horizontalLayout_2);
+        verticalLayout_3.addLayout(horizontalLayout);
 
-        horizontalLayout = new QHBoxLayout();
-        horizontalLayout.setObjectName("horizontalLayout");
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2.setObjectName("horizontalLayout_2");
         btnLogin = new QPushButton(MainLogin);
         btnLogin.setObjectName("btnLogin");
 
-        horizontalLayout.addWidget(btnLogin);
+        horizontalLayout_2.addWidget(btnLogin);
 
         btnExit = new QPushButton(MainLogin);
         btnExit.setObjectName("btnExit");
 
-        horizontalLayout.addWidget(btnExit);
+        horizontalLayout_2.addWidget(btnExit);
 
 
-        verticalLayout_3.addLayout(horizontalLayout);
+        verticalLayout_3.addLayout(horizontalLayout_2);
 
 
         gridLayout.addLayout(verticalLayout_3, 0, 0, 1, 1);
@@ -99,6 +107,5 @@ public class Ui_MainLogin implements com.trolltech.qt.QUiForm<QWidget>
         btnLogin.setText(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Login", null));
         btnExit.setText(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Exit", null));
     } // retranslateUi
-
 }
 
