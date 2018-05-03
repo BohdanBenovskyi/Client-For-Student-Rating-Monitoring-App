595e0a71e18143cc414e0cfac400a968c26e512d
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-05-03 20:41:56 +0300

    MainForm.java

--- src/com/benovskyi/bohdan/Ui_MainForm.java
+++ src/com/benovskyi/bohdan/Ui_MainForm.java
@@ -5,6 +5,7 @@
  **
  ** WARNING! All changes made in this file will be lost when recompiling ui file!
  ********************************************************************************/
+package com.benovskyi.bohdan;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
 
