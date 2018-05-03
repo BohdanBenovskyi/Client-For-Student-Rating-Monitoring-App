43f53603c0c3786d1e5b2f4d90143a8e04425628
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-05-03 16:15:38 +0300

    receive request about l&p

--- src/com/benovskyi/bohdan/Client.java
+++ src/com/benovskyi/bohdan/Client.java
@@ -15,65 +15,71 @@ import java.net.UnknownHostException;
 
 import javax.swing.JOptionPane;
 
-public class Client extends QWidget{
+public class Client extends QWidget {
 
 	public Ui_MainLogin uiLogin = new Ui_MainLogin();
 	public InetAddress addr;
 	public Socket socket;
 	private BufferedReader in;
 	private PrintWriter out;
-	
+
 	public Client() {
 		uiLogin.setupUi(this);
 		uiLogin.btnLogin.clicked.connect(this, "login()");
 		try {
-			addr = InetAddress.getByName(null);	 //127.0.0.1
+			addr = InetAddress.getByName(null); // 127.0.0.1
 			socket = new Socket(addr, 9191);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		try {
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-	      }
-	      catch (IOException e) {
-	         try {
-	            socket.close();
-	         }
-	         catch (IOException e2) {
-	            System.err.println("Socket not closed");
-	         }
-	      }
-		
+			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+		} catch (IOException e) {
+			try {
+				socket.close();
+			} catch (IOException e2) {
+				System.err.println("Socket not closed");
+			}
+		}
+
 		show();
 	}
-	
+
 	public static void main(String[] args) {
 		QApplication.initialize(args);
-		
+
 		Client client = new Client();
-		
+
 		QApplication.execStatic();
 	}
-	
+
 	public void login() {
 		String login = uiLogin.edtLogin.text();
 		String password = uiLogin.edtPassword.text();
-		
-		if(login.isEmpty() || password.isEmpty()) {
-			JOptionPane.showMessageDialog(null, "Some field are empty!\nPlease enter login or password!", "Warning", JOptionPane.WARNING_MESSAGE);
-		}
-		else {
+
+		if (login.isEmpty() || password.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Some field are empty!\nPlease enter login or password!", "Warning",
+					JOptionPane.WARNING_MESSAGE);
+		} else {
 			System.out.println("login: " + login);
 			System.out.println("password: " + password);
-			//out.println(login + " " + password);
+			// out.println(login + " " + password);
 			out.println("login");
 			out.println(login);
 			out.println(password);
-			out.println("END" );
+			
+			try {
+				if(in.readLine().equals("OK")) {
+					System.out.println("All is fine");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			//out.println("END");
 		}
 	}
 
