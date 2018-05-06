903fb984db0ed6a85dd323b5f89b9f99a3b3ee1e
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-05-06 20:01:19 +0300

    login window form

--- src/com/benovskyi/bohdan/Ui_MainLogin.java
+++ src/com/benovskyi/bohdan/Ui_MainLogin.java
@@ -6,107 +6,102 @@
  ** WARNING! All changes made in this file will be lost when recompiling ui file!
  ********************************************************************************/
 package com.benovskyi.bohdan;
+
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
 
-public class Ui_MainLogin implements com.trolltech.qt.QUiForm<QMainWindow>
-{
-    public QWidget centralwidget;
-    public QGridLayout gridLayout;
-    public QVBoxLayout verticalLayout_3;
-    public QHBoxLayout horizontalLayout;
-    public QVBoxLayout verticalLayout;
-    public QLabel lblLogin;
-    public QLabel lblPasswod;
-    public QVBoxLayout verticalLayout_2;
-    public QLineEdit edtLogin;
-    public QLineEdit edtPasswod;
-    public QHBoxLayout horizontalLayout_2;
-    public QPushButton btnLogin;
-    public QPushButton btnClose;
+public class Ui_MainLogin implements com.trolltech.qt.QUiForm<QMainWindow> {
+	public QWidget centralwidget;
+	public QGridLayout gridLayout;
+	public QVBoxLayout verticalLayout_3;
+	public QHBoxLayout horizontalLayout;
+	public QVBoxLayout verticalLayout;
+	public QLabel lblLogin;
+	public QLabel lblPasswod;
+	public QVBoxLayout verticalLayout_2;
+	public QLineEdit edtLogin;
+	public QLineEdit edtPasswod;
+	public QHBoxLayout horizontalLayout_2;
+	public QPushButton btnLogin;
+	public QPushButton btnClose;
 
-    public Ui_MainLogin() { super(); }
+	public Ui_MainLogin() {
+		super();
+	}
 
-    public void setupUi(QMainWindow MainLogin)
-    {
-        MainLogin.setObjectName("MainLogin");
-        MainLogin.resize(new QSize(260, 101).expandedTo(MainLogin.minimumSizeHint()));
-        MainLogin.setWindowIcon(new QIcon(new QPixmap("D:\\Projects\\ClientAppSRM\\resources\\220px-Polytechnic_Lwow_20091.bmp")));
-        centralwidget = new QWidget(MainLogin);
-        centralwidget.setObjectName("centralwidget");
-        gridLayout = new QGridLayout(centralwidget);
-        gridLayout.setObjectName("gridLayout");
-        verticalLayout_3 = new QVBoxLayout();
-        verticalLayout_3.setObjectName("verticalLayout_3");
-        horizontalLayout = new QHBoxLayout();
-        horizontalLayout.setObjectName("horizontalLayout");
-        verticalLayout = new QVBoxLayout();
-        verticalLayout.setObjectName("verticalLayout");
-        lblLogin = new QLabel(centralwidget);
-        lblLogin.setObjectName("lblLogin");
+	public void setupUi(QMainWindow MainLogin) {
+		MainLogin.setObjectName("MainLogin");
+		MainLogin.resize(new QSize(260, 101).expandedTo(MainLogin.minimumSizeHint()));
+		MainLogin.setWindowIcon(
+				new QIcon(new QPixmap("D:\\Projects\\ClientAppSRM\\resources\\220px-Polytechnic_Lwow_20091.bmp")));
+		centralwidget = new QWidget(MainLogin);
+		centralwidget.setObjectName("centralwidget");
+		gridLayout = new QGridLayout(centralwidget);
+		gridLayout.setObjectName("gridLayout");
+		verticalLayout_3 = new QVBoxLayout();
+		verticalLayout_3.setObjectName("verticalLayout_3");
+		horizontalLayout = new QHBoxLayout();
+		horizontalLayout.setObjectName("horizontalLayout");
+		verticalLayout = new QVBoxLayout();
+		verticalLayout.setObjectName("verticalLayout");
+		lblLogin = new QLabel(centralwidget);
+		lblLogin.setObjectName("lblLogin");
 
-        verticalLayout.addWidget(lblLogin);
+		verticalLayout.addWidget(lblLogin);
 
-        lblPasswod = new QLabel(centralwidget);
-        lblPasswod.setObjectName("lblPasswod");
+		lblPasswod = new QLabel(centralwidget);
+		lblPasswod.setObjectName("lblPasswod");
 
-        verticalLayout.addWidget(lblPasswod);
+		verticalLayout.addWidget(lblPasswod);
 
+		horizontalLayout.addLayout(verticalLayout);
 
-        horizontalLayout.addLayout(verticalLayout);
+		verticalLayout_2 = new QVBoxLayout();
+		verticalLayout_2.setObjectName("verticalLayout_2");
+		edtLogin = new QLineEdit(centralwidget);
+		edtLogin.setObjectName("edtLogin");
 
-        verticalLayout_2 = new QVBoxLayout();
-        verticalLayout_2.setObjectName("verticalLayout_2");
-        edtLogin = new QLineEdit(centralwidget);
-        edtLogin.setObjectName("edtLogin");
+		verticalLayout_2.addWidget(edtLogin);
 
-        verticalLayout_2.addWidget(edtLogin);
+		edtPasswod = new QLineEdit(centralwidget);
+		edtPasswod.setObjectName("edtPasswod");
+		edtPasswod.setEchoMode(com.trolltech.qt.gui.QLineEdit.EchoMode.Password);
 
-        edtPasswod = new QLineEdit(centralwidget);
-        edtPasswod.setObjectName("edtPasswod");
-        edtPasswod.setEchoMode(com.trolltech.qt.gui.QLineEdit.EchoMode.Password);
+		verticalLayout_2.addWidget(edtPasswod);
 
-        verticalLayout_2.addWidget(edtPasswod);
+		horizontalLayout.addLayout(verticalLayout_2);
 
+		verticalLayout_3.addLayout(horizontalLayout);
 
-        horizontalLayout.addLayout(verticalLayout_2);
+		horizontalLayout_2 = new QHBoxLayout();
+		horizontalLayout_2.setObjectName("horizontalLayout_2");
+		btnLogin = new QPushButton(centralwidget);
+		btnLogin.setObjectName("btnLogin");
 
+		horizontalLayout_2.addWidget(btnLogin);
 
-        verticalLayout_3.addLayout(horizontalLayout);
+		btnClose = new QPushButton(centralwidget);
+		btnClose.setObjectName("btnClose");
 
-        horizontalLayout_2 = new QHBoxLayout();
-        horizontalLayout_2.setObjectName("horizontalLayout_2");
-        btnLogin = new QPushButton(centralwidget);
-        btnLogin.setObjectName("btnLogin");
+		horizontalLayout_2.addWidget(btnClose);
 
-        horizontalLayout_2.addWidget(btnLogin);
+		verticalLayout_3.addLayout(horizontalLayout_2);
 
-        btnClose = new QPushButton(centralwidget);
-        btnClose.setObjectName("btnClose");
+		gridLayout.addLayout(verticalLayout_3, 0, 0, 1, 1);
 
-        horizontalLayout_2.addWidget(btnClose);
+		MainLogin.setCentralWidget(centralwidget);
+		retranslateUi(MainLogin);
+		btnClose.clicked.connect(MainLogin, "close()");
 
+		MainLogin.connectSlotsByName();
+	} // setupUi
 
-        verticalLayout_3.addLayout(horizontalLayout_2);
-
-
-        gridLayout.addLayout(verticalLayout_3, 0, 0, 1, 1);
-
-        MainLogin.setCentralWidget(centralwidget);
-        retranslateUi(MainLogin);
-        btnClose.clicked.connect(MainLogin, "close()");
-
-        MainLogin.connectSlotsByName();
-    } // setupUi
-
-    void retranslateUi(QMainWindow MainLogin)
-    {
-        MainLogin.setWindowTitle(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Login&Password", null));
-        lblLogin.setText(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Login", null));
-        lblPasswod.setText(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Password", null));
-        btnLogin.setText(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Login", null));
-        btnClose.setText(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Close", null));
-    } // retranslateUi
+	void retranslateUi(QMainWindow MainLogin) {
+		MainLogin.setWindowTitle(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Login&Password", null));
+		lblLogin.setText(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Login", null));
+		lblPasswod.setText(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Password", null));
+		btnLogin.setText(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Login", null));
+		btnClose.setText(com.trolltech.qt.core.QCoreApplication.translate("MainLogin", "Close", null));
+	} // retranslateUi
 
 }
-
