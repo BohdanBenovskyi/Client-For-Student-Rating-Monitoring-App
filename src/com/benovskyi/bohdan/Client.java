3bd992fcf6b3c0fbf6b8139b83656a74fba5b9ca
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-05-06 19:59:27 +0300

    receive list of groups and push it in UI and sort it by institutes

--- src/com/benovskyi/bohdan/Client.java
+++ src/com/benovskyi/bohdan/Client.java
@@ -14,6 +14,7 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
@@ -64,6 +65,7 @@ public class Client extends QMainWindow {
 		String login = uiLogin.edtLogin.text();
 		String password = uiLogin.edtPasswod.text();
 		String institutes = "";
+		String group = "";
 
 		if (login.isEmpty() || password.isEmpty()) {
 			JOptionPane.showMessageDialog(null, "Some field are empty!\nPlease enter login or password!", "Warning",
@@ -89,19 +91,69 @@ public class Client extends QMainWindow {
 						System.out.println("Institutes: " + institutes);
 					}
 
-					//int hmi = countWordsUsingSplit(institutes);
-					//System.out.println("HMI: " + hmi);
 					String[] inst = institutes.split("\\s+");
+
+					String[] groups = new String[inst.length];
+
+					ArrayList allGroups = new ArrayList();
+
+					if (in.readLine().equals("grp")) {
+						for (int i = 0; i < groups.length; i++) {
+							groups[i] = in.readLine();
+						}
+					}
+
+					for (int i = 0; i < inst.length; i++) {
+						allGroups.add(i);
+						String[] tmp = groups[i].split("\\s+");
+						for (int j = 0; j < tmp.length; j++)
+							allGroups.add(tmp[j]);
+					}
 
 					QMenu instit = new QMenu("Інститути");
 					QAction quit = new QAction("Вихід", this);
+
+					QMenu[] insti = new QMenu[inst.length];
+
+					for (int i = 0; i < inst.length; i++) {
+						insti[i] = new QMenu(inst[i], this);
+					}
 					
+					/*WORK!!!!!*/
+					QAction[] gr = new QAction[allGroups.size() - inst.length];
+					boolean flag = false;
+					boolean flg = false;
+					int tmp = 0;
 					for(int i = 0; i < inst.length; i++) {
-						instit.addMenu(new QMenu(inst[i], this));
-						
+						for(int j = 0; j < allGroups.size(); j++) {
+							if(allGroups.get(j) instanceof Integer)
+								if(!flag) {
+									allGroups.remove(j);
+									flag = true;
+									flg = true;
+									tmp = j;
+								}
+							if(allGroups.get(j) instanceof Integer) {
+								allGroups.remove(j);
+								tmp = j;
+								flg = true;
+								break;
+							}
+							else {
+								if(flg) {
+									j = tmp;
+									flg = false;
+								}
+								gr[j] = new QAction(allGroups.get(j).toString(), this);
+								insti[i].addAction(gr[j]);
+							}
+						}
 					}
 
-					uiForm.menuFile.addMenu(instit);
+					for (int l = 0; l < inst.length; l++) {
+						uiForm.menuFile.addMenu(insti[l]);
+					}
+
 					uiForm.menuFile.addAction(quit);
 					quit.triggered.connect(QApplication.instance(), "quit()");
 				}
@@ -110,6 +162,7 @@ public class Client extends QMainWindow {
 			}
 			// out.println("END");
 		}
+
 	}
 
 	public static int countWordsUsingSplit(String input) {
